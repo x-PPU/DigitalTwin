fd51c6d823a8a0bfd5c21e53edd2005daf3f1f91
JiayangLi7 <li.jiayang@myyahoo.com>
2026-07-06 20:53:45 +0800

    7-6

--- Different Files Processing Times/plot.py
+++ Different Files Processing Times/plot.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-categories = ['Electric CAD\n(ECAD)', 'Datasheets\n(PDF)', 'CAD models\n(STP/STEP)',
-              'PLC programs\n(PLCOpenXML)', 'SysML models\n(UML)',
-              'MATLAB/Simulink models\n (SLX)', 'Bills Of Materials\n(CSV)',
-              'Technical Reports\n(PDF)']
-automatic = [2.864, 7.47291176470588, 14.77388, 53.273, 798.092, 33.999, 1.725, 0]
-manual_actual = [0, 0, 0, 0, 0, 7200, 36000, 72000]
+categories = ['BOM\n(CSV)\nComponent AAS\nSM-Capability\nSM-MechanicalBreakdown', 'Datasheets\n(PDF)\nComponent AAS', 'CAD\n(STP/STEP)\nSM-Geometry',
+              'PLC\n(PLCOpenXML)\nSM-PLC\nSM-OperationalData', 'SysML\n(UML)\nSM-Behavior',
+              'MATLAB/Simulink\n (SLX)\nSM-Simulation', 'ECAD\n(E3S)\nSM-ECAD',  
+              'Technical Reports\n(PDF)\nBOM']
+automatic = [1.725, 7.47291176470588, 14.77388, 53.273, 798.092, 33.999, 2.864, 0]
+manual_actual = [0, 0, 0, 0, 0, 7200, 12000, 36000]
 manual_display = [man if man > 0 else 0 for man in manual_actual]
 x = np.arange(len(categories))
-width = 0.6
+width = 0.5
 base = 0.1
 
 fig, ax = plt.subplots(figsize=(14, 7))
@@ -25,22 +25,22 @@ max_total = max([base + a + m for a, m in zip(automatic, manual_display)])
 ax.set_ylim(0.09, max_total * 1.2)
 ax.yaxis.set_major_formatter(plt.ScalarFormatter())
 ax.yaxis.set_major_locator(plt.FixedLocator([0.1, 1, 10, 100, 1000, 10000, 100000]))
-ax.tick_params(axis='both', labelsize=11)
+ax.tick_params(axis='both', labelsize=20)
 
 for i, (auto, wh) in enumerate(zip(automatic, manual_display)):
     if auto > 0:
         y_pos = base + auto / 2
         ax.text(i, y_pos, f'{auto:.2f}', ha='center', va='center',
-                color='black', fontsize=10, fontweight='bold')
+                color='black', fontsize=20, fontweight='bold')
     if wh > 0:
         y_pos = base + auto + wh / 2
         ax.text(i, y_pos, '', ha='center', va='center',
-                color='black', fontsize=14, fontweight='bold')
+                color='black', fontsize=200, fontweight='bold')
 
-ax.set_ylabel('time in s', fontsize=13)
+ax.set_ylabel('time in s', fontsize=18)
 ax.set_xticks(x)
-ax.set_xticklabels(categories, fontsize=10)
-ax.legend(loc='upper left', fontsize=12)
+ax.set_xticklabels(categories, fontsize=17)
+ax.legend(loc='upper left', fontsize=20)
 
 # footnote
 #fig.text(0.02, 0.01, 'manual:', ha='left', va='bottom',
@@ -50,5 +50,5 @@ ax.legend(loc='upper left', fontsize=12)
 
 ax.grid(True, which='major', axis='y', linestyle='--', alpha=0.5)
 plt.tight_layout()
-plt.subplots_adjust(bottom=0.12)
+
 plt.show()
